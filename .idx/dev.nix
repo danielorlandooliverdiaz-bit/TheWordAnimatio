5482df5308e0059ad055f81cc6a3d5aac390048c
uzumakib554 <uzumakib554@gmail.com>
2025-10-07 11:42:30 +0000

    nix config file changes

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,10 +1,16 @@
 { pkgs, ... }: {
+  # The list of packages to be installed
+  # in your environment.
+  packages = [
+    pkgs.python3
+  ];
+
   # Let Project IDX know you're running a web server.
   previews = {
     enable = true;
     previews = [{
       # A simple Python server that serves the current directory.
-      command = [ "python" "-m" "http.server" "$PORT" ];
+      command = [ "python3" "-m" "http.server" "$PORT" ];
       manager = "web";
     }];
   };
