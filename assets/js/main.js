bb373ac4734924827e972fd40de047b6ea90b566
uzumakib554 <uzumakib554@gmail.com>
2025-10-14 17:29:12 +0000

    lol

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,4 +20,22 @@ $(document).ready(function() {
         // Asegurarse de que el enlace correspondiente en la barra de navegación principal se marque como activo
         $('.navbar-nav a[href="' + target + '"]').addClass('active');
     });
+
+    // Función para mostrar/ocultar detalles de los proyectos
+    $('.toggle-details').click(function(e) {
+        e.preventDefault();
+
+        // Encuentra el div con los detalles dentro de la misma tarjeta
+        var details = $(this).siblings('.project-details');
+        
+        // Muestra u oculta los detalles con una animación
+        details.slideToggle(400);
+
+        // Cambia el texto del botón
+        if ($(this).text() == "Leer más") {
+            $(this).text("Leer menos");
+        } else {
+            $(this).text("Leer más");
+        }
+    });
 });
