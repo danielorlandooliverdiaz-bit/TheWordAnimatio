d8247bf0eb690ddbbef33754ce1346240a36b213
uzumakib554 <uzumakib554@gmail.com>
2025-10-14 17:40:51 +0000

    dasd

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,37 +5,43 @@ $(document).ready(function() {
 
     // Función para manejar los clics en la navegación
     $('.navbar-nav a, .navbar-brand, .hero a').click(function(e) {
-        // Prevenir el comportamiento por defecto del enlace
         e.preventDefault();
-
-        // Obtener el href del enlace (ej. #inicio, #producciones)
         var target = $(this).attr('href');
-
-        // Ocultar todas las secciones y luego mostrar la seleccionada
         $('.content-section').hide();
         $(target).fadeIn(400);
-
-        // Actualizar la clase 'active' en la navegación
         $('.navbar-nav .nav-link').removeClass('active');
-        // Asegurarse de que el enlace correspondiente en la barra de navegación principal se marque como activo
         $('.navbar-nav a[href="' + target + '"]').addClass('active');
     });
 
     // Función para mostrar/ocultar detalles de los proyectos
     $('.toggle-details').click(function(e) {
         e.preventDefault();
-
-        // Encuentra el div con los detalles dentro de la misma tarjeta
         var details = $(this).siblings('.project-details');
-        
-        // Muestra u oculta los detalles con una animación
         details.slideToggle(400);
-
-        // Cambia el texto del botón
         if ($(this).text() == "Leer más") {
             $(this).text("Leer menos");
         } else {
             $(this).text("Leer más");
+        }
+    });
+
+    // Validación del formulario de contacto y modal
+    $('#contact-form').submit(function(e) {
+        e.preventDefault(); // Prevenir el envío real del formulario
+
+        var email = $('#contact-email').val();
+        var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+        if (emailRegex.test(email)) {
+            // Si el email es válido, mostrar el modal
+            var confirmationModal = new bootstrap.Modal(document.getElementById('confirmationModal'));
+            confirmationModal.show();
+
+            // Limpiar el formulario
+            $(this)[0].reset();
+        } else {
+            // Si el email no es válido, mostrar una alerta
+            alert('Por favor, introduce una dirección de correo electrónico válida.');
         }
     });
 });
